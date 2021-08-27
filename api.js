01fa937fbc8b0e934ac56e7622791d944e24bb2b
Elwyn Ynion <elwynynionpersonal@gmail.com>
2021-08-27 14:46:57 +0800

    🔒️ Code converted to obfuscator.io

---
+++ api.js
@@ -0,0 +1,32 @@
+// I use obfuscator for JavaScript for some reason
+function _0x55c1() {
+    const _0x2764fb = ['UC-lHJZR3Gqxm24_Vd_AJ5Yw', '6ZbtjyR', '10EEfwHu', '151629wTcpuY', '230890anlGEw', '202167bSaVuk', '4200920Spmsdx', '3015803WTaynO', '1267745bMUcHT', '200UlPeFm', '749352FVXFUA', '18qEKUCP', '1332xrmeUr'];
+    _0x55c1 = function () {
+        return _0x2764fb;
+    };
+    return _0x55c1();
+}
+const _0x497321 = _0x3ce2;
+
+function _0x3ce2(_0x3f6b3e, _0x1e75ce) {
+    const _0x55c128 = _0x55c1();
+    return _0x3ce2 = function (_0x3ce29a, _0x4a894b) {
+        _0x3ce29a = _0x3ce29a - 0x6a;
+        let _0x4a2296 = _0x55c128[_0x3ce29a];
+        return _0x4a2296;
+    }, _0x3ce2(_0x3f6b3e, _0x1e75ce);
+}(function (_0x3d4310, _0x204f2d) {
+    const _0x5a6e0c = _0x3ce2,
+        _0x57cef6 = _0x3d4310();
+    while (!![]) {
+        try {
+            const _0x2dde16 = -parseInt(_0x5a6e0c(0x70)) / 0x1 + -parseInt(_0x5a6e0c(0x73)) / 0x2 * (-parseInt(_0x5a6e0c(0x6b)) / 0x3) + parseInt(_0x5a6e0c(0x72)) / 0x4 * (-parseInt(_0x5a6e0c(0x6a)) / 0x5) + -parseInt(_0x5a6e0c(0x76)) / 0x6 * (parseInt(_0x5a6e0c(0x6f)) / 0x7) + parseInt(_0x5a6e0c(0x6e)) / 0x8 + parseInt(_0x5a6e0c(0x6d)) / 0x9 * (-parseInt(_0x5a6e0c(0x71)) / 0xa) + -parseInt(_0x5a6e0c(0x6c)) / 0xb * (-parseInt(_0x5a6e0c(0x74)) / 0xc);
+            if (_0x2dde16 === _0x204f2d) break;
+            else _0x57cef6['push'](_0x57cef6['shift']());
+        } catch (_0x506cfa) {
+            _0x57cef6['push'](_0x57cef6['shift']());
+        }
+    }
+}(_0x55c1, 0xc03b6));
+const youtubeKey = 'AIzaSyCJqIKQAS61I9jARpJ2h-XhZGCHayvunaE',
+    youtubeUser = _0x497321(0x75);
